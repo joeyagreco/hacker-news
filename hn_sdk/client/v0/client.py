ef51ddbc6207c77e61ba92be0e48026100fcf772
joeyagreco <joeyagreco@gmail.com>
2024-02-19 22:00:49 -0600

    stuff

--- hn_sdk/client/v0/client.py
+++ hn_sdk/client/v0/client.py
@@ -17,23 +17,21 @@ MAX_ITEM_PATH = "maxitem"
 UPDATES_PATH = "updates"
 
 
-def get_item(item_id: str) -> dict:
+def get_item_by_id(item_id: str) -> dict:
     """
     https://github.com/HackerNews/API?tab=readme-ov-file#items
     """
     url = f"{BASE_URL}/{VERSION}/{ITME_PATH}/{item_id}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
-def get_user(username: str) -> dict:
+def get_user_by_username(username: str) -> dict:
     """
     https://github.com/HackerNews/API?tab=readme-ov-file#users
     """
     url = f"{BASE_URL}/{VERSION}/{USER_PATH}/{username}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
@@ -43,7 +41,6 @@ def get_max_item() -> dict:
     """
     url = f"{BASE_URL}/{VERSION}/{MAX_ITEM_PATH}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
@@ -53,7 +50,6 @@ def get_top_stories() -> dict:
     """
     url = f"{BASE_URL}/{VERSION}/{TOP_STORIES_PATH}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
@@ -63,7 +59,6 @@ def get_new_stories() -> dict:
     """
     url = f"{BASE_URL}/{VERSION}/{NEW_STORIES_PATH}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
@@ -73,7 +68,6 @@ def get_best_stories() -> dict:
     """
     url = f"{BASE_URL}/{VERSION}/{BEST_STORIES_PATH}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
@@ -83,7 +77,6 @@ def get_ask_stories() -> dict:
     """
     url = f"{BASE_URL}/{VERSION}/{ASK_STORIES_PATH}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
@@ -93,7 +86,6 @@ def get_show_stories() -> dict:
     """
     url = f"{BASE_URL}/{VERSION}/{SHOW_STORIES_PATH}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
@@ -103,7 +95,6 @@ def get_job_stories() -> dict:
     """
     url = f"{BASE_URL}/{VERSION}/{JOB_STORIES_PATH}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
 
 
@@ -113,5 +104,4 @@ def get_updates() -> dict:
     """
     url = f"{BASE_URL}/{VERSION}/{UPDATES_PATH}.json"
     response = rest_call(requests.get, url)
-    response.raise_for_status()
     return response.json()
