1066b2e750cec47518012524bfd912afb59cb30f
joeyagreco <joeyagreco@gmail.com>
2024-02-19 22:38:49 -0600

    stuff

--- hn_sdk/client/v0/client.py
+++ hn_sdk/client/v0/client.py
@@ -35,7 +35,7 @@ def get_user_by_username(username: str) -> dict:
     return response.json()
 
 
-def get_max_item() -> dict:
+def get_max_item_id() -> dict:
     """
     https://github.com/HackerNews/API?tab=readme-ov-file#live-data
     """
--- test_e2e/test_client/v0/test_client.py
+++ test_e2e/test_client/v0/test_client.py
@@ -1,11 +1,22 @@
 import time
 import unittest
 
-from hn_sdk.client.v0.client import get_item_by_id
+from hn_sdk.client.v0.client import (
+    get_ask_stories,
+    get_best_stories,
+    get_item_by_id,
+    get_job_stories,
+    get_max_item_id,
+    get_new_stories,
+    get_show_stories,
+    get_top_stories,
+    get_updates,
+    get_user_by_username,
+)
 
 
 class TestClient(unittest.TestCase):
-    __DELAY_BETWEEN_TESTS = 0.25
+    __DELAY_BETWEEN_TESTS = 0.5
     __RESPONSE_REQUIRED_KEYS = ["id"]
     __TEST_STORY_ID = 8863
     __TEST_COMMENT_ID = 2921983
@@ -13,6 +24,7 @@ class TestClient(unittest.TestCase):
     __TEST_JOB_ID = 192327
     __TEST_POLL_ID = 126809
     __TEST_ITEM_PART_ID = 160705
+    __TEST_USERNAME = "jl"
 
     def setUp(self) -> None:
         # sleep before each test to avoid hitting rate limits.
@@ -56,3 +68,62 @@ class TestClient(unittest.TestCase):
         resp = get_item_by_id(self.__TEST_ITEM_PART_ID)
         self.assertIsInstance(resp, dict)
         self.assertTrue(self.__required_keys_in_response(resp))
+
+    def test_get_user_by_username(self):
+        resp = get_user_by_username(self.__TEST_USERNAME)
+        self.assertIsInstance(resp, dict)
+        self.assertTrue(self.__required_keys_in_response(resp))
+
+    def test_get_max_item_id(self):
+        resp = get_max_item_id()
+        self.assertIsInstance(resp, int)
+
+    def test_get_top_stories(self):
+        resp = get_top_stories()
+        self.assertIsInstance(resp, list)
+        # NOTE: can be up to 500
+        self.assertTrue(len(resp) >= 1)
+        self.assertIsInstance(resp[0], int)
+
+    def test_get_new_stories(self):
+        resp = get_new_stories()
+        self.assertIsInstance(resp, list)
+        # NOTE: can be up to 500
+        self.assertTrue(len(resp) >= 1)
+        self.assertIsInstance(resp[0], int)
+
+    def test_get_best_stories(self):
+        resp = get_best_stories()
+        self.assertIsInstance(resp, list)
+        # NOTE: can be up to 500
+        self.assertTrue(len(resp) >= 1)
+        self.assertIsInstance(resp[0], int)
+
+    def test_get_ask_stories(self):
+        resp = get_ask_stories()
+        self.assertIsInstance(resp, list)
+        # NOTE: can be up to 200
+        self.assertTrue(len(resp) >= 1)
+        self.assertIsInstance(resp[0], int)
+
+    def test_get_show_stories(self):
+        resp = get_show_stories()
+        self.assertIsInstance(resp, list)
+        # NOTE: can be up to 200
+        self.assertTrue(len(resp) >= 1)
+        self.assertIsInstance(resp[0], int)
+
+    def test_get_job_stories(self):
+        resp = get_job_stories()
+        self.assertIsInstance(resp, list)
+        # NOTE: can be up to 200
+        self.assertTrue(len(resp) >= 1)
+        self.assertIsInstance(resp[0], int)
+
+    def test_get_updates(self):
+        resp = get_updates()
+        self.assertIsInstance(resp, dict)
+        self.assertTrue("items" in resp.keys())
+        self.assertIsInstance(resp["items"], list)
+        self.assertTrue("profiles" in resp.keys())
+        self.assertIsInstance(resp["profiles"], list)
